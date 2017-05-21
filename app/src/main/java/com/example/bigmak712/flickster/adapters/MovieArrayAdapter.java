5001a07bb9fde34c2276a7563bb35619a0baacca
bigmak712 <bigmak712@gmail.com>
2017-05-21 12:19:20 -0700

    Implemented ViewHolder Pattern for the MovieArrayAdapter

--- app/src/main/java/com/example/bigmak712/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/bigmak712/flickster/adapters/MovieArrayAdapter.java
@@ -18,11 +18,21 @@ import java.util.List;
 
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
+import static com.example.bigmak712.flickster.R.id.tvOverview;
+import static com.example.bigmak712.flickster.R.id.tvTitle;
+
 /**
  * Created by bigmak712 on 5/13/17.
  */
 
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
+
+    // View lookup cache
+    private static class ViewHolder {
+        ImageView movieImage;
+        TextView title;
+        TextView overview;
+    }
 
     private boolean portraitOrientation = true;
 
@@ -37,15 +47,38 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        // get the data item for position
+
+        // Get the data item for position
         Movie movie = getItem(position);
 
-        // check the existing view being reused
+        // View lookup cache stored in tag
+        ViewHolder viewHolder;
+
+        // Check if an existing view is being reused, otherwise inflate the view
         if(convertView == null) {
+
+            // If there's no view to re-use, inflate a brand new view for row
+            viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            // Set the fields in the viewHolder
+            viewHolder.movieImage = (ImageView)convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.title = (TextView)convertView.findViewById(tvTitle);
+            viewHolder.overview = (TextView)convertView.findViewById(tvOverview);
+
+            // Cache the viewHolder object inside the fresh view
+            convertView.setTag(viewHolder);
+        }
+        else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
+        viewHolder.title.setText(movie.getOriginalTitle());
+        viewHolder.overview.setText(movie.getOverview());
+
+        /*
         // find the image view
         ImageView ivImage = (ImageView)convertView.findViewById(R.id.ivMovieImage);
         // clear out image from image view
@@ -57,18 +90,18 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         // populate data
         tvTitle.setText(movie.getOriginalTitle());
         tvOverview.setText(movie.getOverview());
+        */
 
-        // set the image based on the orientation
-        // round out the image corners
+        // Set the image based on the orientation & round out the image corners
         if(getPortraitOrientation()) {
             //Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
             Picasso.with(getContext()).load(movie.getPosterPath()).
-                    transform(new RoundedCornersTransformation(10, 10)).into(ivImage);
+                    transform(new RoundedCornersTransformation(10, 10)).into(viewHolder.movieImage);
         }
         else {
             //Picasso.with(getContext()).load(movie.getBackdropPath()).into(ivImage);
             Picasso.with(getContext()).load(movie.getBackdropPath()).
-                    transform(new RoundedCornersTransformation(10, 10)).into(ivImage);
+                    transform(new RoundedCornersTransformation(10, 10)).into(viewHolder.movieImage);
         }
 
         // return the view
